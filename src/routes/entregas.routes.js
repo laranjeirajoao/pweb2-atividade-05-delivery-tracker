61a47c56a4d886fd6c1a612f48d1626dfb50b307
João Laranjeira <jpclr1@aluno.ifal.edu.br>
2026-03-19 20:13:48 -0300

    feat: adiciona novas rotas, que utilizam o controller

--- src/routes/entregas.routes.js
+++ src/routes/entregas.routes.js
@@ -1,6 +1,18 @@
 import { Router } from "express";
+import { EntregasController } from "../controllers/entregas.controller.js";
+import { Database } from "../database/database.js";
+import { EntregasRepository } from "../repositories/entregas.repository.js";
+import { EntregasService } from "../services/entregas.service.js";
+
+const database = new Database()
+const repository = new EntregasRepository(database);
+const service = new EntregasService(repository);
+const controller = new EntregasController(service);
 
 const router = Router()
 
+router.get('/', controller.listarTodos);
+router.get('/:id', controller.buscarPorId);
+router.post('/', controller.criar);
 
 export default router

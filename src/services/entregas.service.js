57b4d3cddd4d825e0659905cb049202746621097
João Laranjeira <jpclr1@aluno.ifal.edu.br>
2026-03-20 11:34:08 -0300

    fix: muda o nome de variaveis de evento para entrega, e aplica a formatacao com o Prettier no arquivo

--- src/services/entregas.service.js
+++ src/services/entregas.service.js
@@ -1,56 +1,62 @@
-import { AppError } from '../utils/AppError.js';
-import { EntregaStatus } from '../utils/EntregaStatusEnum.js';
+import { AppError } from "../utils/AppError.js";
+import { EntregaStatus } from "../utils/EntregaStatusEnum.js";
 
 export class EntregasService {
-   constructor(repository) {
-      this.repository = repository; // Dependência injetada
-   }
+	constructor(repository) {
+		this.repository = repository; // Dependência injetada
+	}
 
-   async listarTodos(status) {
-      if (status) {
-         const items = await this.repository.listarTodos()
-         return items.filter(x => x.status === status)
-      }
-      return this.repository.listarTodos();
-   }
+	async listarTodos(status) {
+		if (status) {
+			const items = await this.repository.listarTodos();
+			return items.filter((x) => x.status === status);
+		}
+		return this.repository.listarTodos();
+	}
 
-   async buscarPorId(id) {
-      const evento = await this.repository.buscarPorId(id);
-      if (!evento) throw new AppError('Usuário não encontrado', 404);
-      return evento;
-   }
+	async buscarPorId(id) {
+		const entrega = await this.repository.buscarPorId(id);
+		if (!entrega) throw new AppError("Usuário não encontrado", 404);
+		return entrega;
+	}
 
-   async criar({ descricao, origem, destino }) {
-      const jaExiste = await this.repository.buscarPorDescricaoOrigemEDestino({ descricao, origem, destino });
-      if (jaExiste) throw new AppError('Entrega já cadastrada', 409);
+	async criar({ descricao, origem, destino }) {
+		const jaExiste = await this.repository.buscarPorDescricaoOrigemEDestino({
+			descricao,
+			origem,
+			destino,
+		});
+		if (jaExiste) throw new AppError("Entrega já cadastrada", 409);
 
-      const novaEntrega = {
-         descricao,
-         origem,
-         destino,
-         status: EntregaStatus.CRIADA,
-         historico: [{
-            data: new Date().toISOString(),
-            descricao: "Criação"
-         }]
-      }
+		const novaEntrega = {
+			descricao,
+			origem,
+			destino,
+			status: EntregaStatus.CRIADA,
+			historico: [
+				{
+					data: new Date().toISOString(),
+					descricao: "Criação",
+				},
+			],
+		};
 
-      return this.repository.criar(novaEntrega);
-   }
+		return this.repository.criar(novaEntrega);
+	}
 
-   async atualizar(id, dados) {
-      await this.buscarPorId(id); // Reutiliza a validação de existência
-      return this.repository.atualizar(id, dados);
-   }
+	async atualizar(id, dados) {
+		await this.buscarPorId(id); // Reutiliza a validação de existência
+		return this.repository.atualizar(id, dados);
+	}
 
-   async remover(id) {
-      await this.buscarPorId(id); // Reutiliza a validação de existência
-      return this.repository.remover(id);
-   }
+	async remover(id) {
+		await this.buscarPorId(id); // Reutiliza a validação de existência
+		return this.repository.remover(id);
+	}
 
-   async buscarHistoricoPorId(id) {
-      const evento = await this.repository.buscarPorId(id);
-      if (!evento) throw new AppError('Usuário não encontrado', 404);
-      return evento.historico;
-   }
+	async buscarHistoricoPorId(id) {
+		const entrega = await this.repository.buscarPorId(id);
+		if (!entrega) throw new AppError("Usuário não encontrado", 404);
+		return entrega.historico;
+	}
 }

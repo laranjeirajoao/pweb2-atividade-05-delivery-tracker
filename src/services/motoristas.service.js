0ff7874ba4df6df03c20d4d627c41bfd154100d7
laranjeirajoao <jpclr1@aluno.ifal.edu.br>
2026-03-28 19:45:02 -0300

    feat: adiciona o service com os metodos basicos de motorista

---
+++ src/services/motoristas.service.js
@@ -0,0 +1,37 @@
+import { AppError } from "../utils/AppError.js";
+import { MotoristaStatus } from "../utils/MotoristaStatusEnum.js";
+
+export class MotoristasService {
+	constructor(repository) {
+		this.repository = repository; // Dependência injetada
+	}
+
+	async listarTodos() {
+		return this.repository.listarTodos();
+	}
+
+	async buscarPorId(id) {
+		const motorista = await this.repository.buscarPorId(id);
+		if (!motorista) throw new AppError("Motorista não encontrado", 404);
+		return motorista;
+	}
+
+	async criar({ nome, cpf, placaVeiculo }) {
+		const jaExiste = await this.repository.buscarPorCpf(cpf);
+		if (jaExiste) throw new AppError("Motorista já cadastrado", 409);
+
+		const novoMotorista = {
+			nome,
+			cpf,
+			placaVeiculo,
+			status: MotoristaStatus.ATIVO,
+		};
+
+		return this.repository.criar(novoMotorista);
+	}
+
+	async atualizar(id, dados) {
+		await this.buscarPorId(id); // Reutiliza a validação de existência
+		return this.repository.atualizar(id, dados);
+	}
+}

98f44e3756954535dbb0e74ae43042cb70cdeef9
laranjeirajoao <jpclr1@aluno.ifal.edu.br>
2026-03-28 21:04:42 -0300

    feat: registra a url /api/motoristas no registro de rotas

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import entregasRouter from "./entregas.routes.js";
+import motoristasRouter from "./motoristas.routes.js";
 
 const router = Router();
 
 router.use("/api/entregas", entregasRouter);
+router.use("/api/motoristas", motoristasRouter);
 
 export default router;

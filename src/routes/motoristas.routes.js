c07f56ff9843fa385138806a760ab2cb6aeafd27
laranjeirajoao <jpclr1@aluno.ifal.edu.br>
2026-03-29 00:26:18 -0300

    refactor: importa atualiza as dependencias de controller, utilizando o novo export do arquivo central

--- src/routes/motoristas.routes.js
+++ src/routes/motoristas.routes.js
@@ -1,20 +1,12 @@
 import { Router } from "express";
-import { MotoristasController } from "../controllers/motoristas.controller.js";
-import { Database } from "../database/database.js";
 import { validarCriacaoMotorista } from "../middlewares/validacao.middleware.js";
-import { MotoristasRepositoryInMemory } from "../repositories/motoristas/motoristas-in-memory.repository.js";
-import { MotoristasService } from "../services/motoristas.service.js";
-
-const database = new Database();
-const repository = new MotoristasRepositoryInMemory(database);
-const service = new MotoristasService(repository);
-const controller = new MotoristasController(service);
+import { motoristasController } from "./composicao-dependencias.js";
 
 const router = Router();
 
-router.post("/", validarCriacaoMotorista, controller.criar);
-router.get("/", controller.listarTodos);
-router.get("/:id", controller.buscarPorId);
-// router.get("/:id/entregas", controller.buscarPorId); todo: discutir implementacao
+router.post("/", validarCriacaoMotorista, motoristasController.criar);
+router.get("/", motoristasController.listarTodos);
+router.get("/:id", motoristasController.buscarPorId);
+// router.get("/:id/entregas", motoristasController.buscarPorId)
 
 export default router;

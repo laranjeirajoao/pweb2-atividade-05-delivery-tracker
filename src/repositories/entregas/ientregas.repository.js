1905aa7b94795a16de24d1f5f8eec4f53aec5f35
laranjeirajoao <jpclr1@aluno.ifal.edu.br>
2026-03-29 16:58:02 -0300

    feat: adicionando jsdoc no contrato dos repositories

--- src/repositories/entregas/ientregas.repository.js
+++ src/repositories/entregas/ientregas.repository.js
@@ -1,4 +1,11 @@
 // Tentando simular algo parecido com uma interface do typescript
+/**
+ * @import { Entrega } from '../../database/database.js'
+ */
+
+/**
+ * Contrato base para o repositorio de Entregas
+ */
 export class IEntregasRepository {
 	constructor() {
 		if (this.constructor == IEntregasRepository) {
@@ -6,15 +13,43 @@ export class IEntregasRepository {
 		}
 	}
 
-	async listarTodos(status) {
+	/**
+	 * Função que busca e retorna um array com todas as entregas
+	 * @abstract
+	 * @param {any} filtros - Opcoes de filtros que podem ser passados
+	 * @returns {Promise<Entrega[]>}
+	 */
+	async listarTodos(filtros) {
 		throw new Error("Não implementado");
 	}
+
+	/**
+	 * Função que recebe um id busca um objeto de id correspondente
+	 * @abstract
+	 * @param {number} id - ID da entrega
+	 * @returns {Promise<Entrega>}
+	 */
 	async buscarPorId(id) {
 		throw new Error("Não implementado");
 	}
-	async criar(entrega) {
+
+	/**
+	 * Função que recebe dados de entregas e persiste-os no banco
+	 * @abstract
+	 * @param {Entrega} dados - Valores que devem ser persistidos no banco
+	 * @returns {Promise<Entrega>}
+	 */
+	async criar(dados) {
 		throw new Error("Não implementado");
 	}
+
+	/**
+	 * Função que recebe um id e dados
+	 * @abstract
+	 * @param {number} id - ID do objeto a ser atualizado
+	 * @param {Entrega} dados - Valores atualizados do objeto
+	 * @returns {Promise<Entrega>}
+	 */
 	async atualizar(id, dados) {
 		throw new Error("Não implementado");
 	}

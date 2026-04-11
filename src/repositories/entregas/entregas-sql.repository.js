cc7fe84266cea6425b2ce23ec6434a66910e53a5
João Laranjeira <jpclr1@aluno.ifal.edu.br>
2026-04-10 21:36:52 -0300

    feat: implementando metodos de busca para o repository com SQL

---
+++ src/repositories/entregas/entregas-sql.repository.js
@@ -0,0 +1,77 @@
+import { pool } from "../../database/postgres.js";
+import { IEntregasRepository } from "./ientregas.repository.js";
+/** @import { IEntregasRepository } from './ientregas.repository.js' */
+
+/**
+ * @extends {IEntregasRepository}
+ */
+export class EntregasRepositorySQL extends IEntregasRepository {
+	async listarTodos({ descricao, origem, destino, status, motoristaId } = {}) {
+		let query = "SELECT * FROM ENTREGAS";
+		let condicoes = [];
+		let params = [];
+
+		if (descricao) {
+			condicoes.push("descricao = ?");
+			params.push(descricao);
+		}
+
+		if (origem) {
+			condicoes.push("origem = ?");
+			params.push(origem);
+		}
+
+		if (destino) {
+			condicoes.push("destino = ?");
+			params.push(destino);
+		}
+
+		if (status) {
+			const lista = Array.isArray(status) ? status : [status];
+			// a idea é criar algo como status in (?, ?) dependendo da quantidade de objetos na lista
+			const placeholders = lista.map(() => "?").join(", ");
+			condicoes.push(`status in (${placeholders})`);
+			params.push(...lista);
+		}
+
+		if (motoristaId) {
+			condicoes.push("motorista_id = ?");
+			params.push(motoristaId);
+		}
+
+		if (condicoes.length > 0) {
+			query += " WHERE " + condicoes.join(" AND ");
+		}
+
+		return (await pool.query(query, params)).rows;
+	}
+
+	async buscarPorId(id) {
+		const { rows } = await pool.query(
+			"SELECT * FROM ENTREGAS WHERE ID = $1",
+			[id],
+		);
+		return rows ?? null;
+	}
+
+	async criar(dados) {
+		const novaEntrega = {
+			...dados,
+			motoristaId: null,
+			id: this.database.generateId(),
+		};
+		this.database.getEntregas().push(novaEntrega);
+		return novaEntrega;
+	}
+
+	async atualizar(id, dados) {
+		const indice = this.database.getEntregas().findIndex((u) => u.id === id);
+		if (indice === -1) return null;
+		this.database.getEntregas()[indice] = {
+			...this.database.getEntregas()[indice],
+			...dados,
+			id,
+		};
+		return this.database.getEntregas()[indice];
+	}
+}

f39bd5825d22a73887a671344b3a2ae3569aa5b1
João Laranjeira <jpclr1@aluno.ifal.edu.br>
2026-04-11 22:59:46 -0300

    feat: troca o repositorio de entregas para o repositorio SQL e passa a utilizar alias no nome dos repositories para mudar apenas o import futuramente

--- src/routes/composicao-dependencias.js
+++ src/routes/composicao-dependencias.js
@@ -2,14 +2,15 @@
 import { EntregasController } from "../controllers/entregas.controller.js";
 import { MotoristasController } from "../controllers/motoristas.controller.js";
 import { Database } from "../database/database.js";
-import { EntregasRepositoryInMemory } from "../repositories/entregas/entregas-in-memory.repository.js";
-import { MotoristasRepositoryInMemory } from "../repositories/motoristas/motoristas-in-memory.repository.js";
+// import { EntregasRepositoryInMemory as EntregasRepository } from "../repositories/entregas/entregas-in-memory.repository.js";
+import { EntregasRepositorySQL as EntregasRepository } from "../repositories/entregas/entregas-sql.repository.js";
+import { MotoristasRepositoryInMemory as MotoristasRepository } from "../repositories/motoristas/motoristas-in-memory.repository.js";
 import { EntregasService } from "../services/entregas.service.js";
 import { MotoristasService } from "../services/motoristas.service.js";
 
 const database = new Database();
-const entregasRepo = new EntregasRepositoryInMemory(database);
-const motoristasRepo = new MotoristasRepositoryInMemory(database);
+const entregasRepo = new EntregasRepository();
+const motoristasRepo = new MotoristasRepository(database);
 const entregasService = new EntregasService(entregasRepo, motoristasRepo);
 const motoristasService = new MotoristasService(motoristasRepo);
 const entregasController = new EntregasController(entregasService);

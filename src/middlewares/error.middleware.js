f87189b51244840be0f2ca95fd5b44f7407ff662
laranjeirajoao <jpclr1@aluno.ifal.edu.br>
2026-04-12 19:41:13 -0300

    feat: adiciona o mapeamento da nova classe de erro no middleware central

--- src/middlewares/error.middleware.js
+++ src/middlewares/error.middleware.js
@@ -1,8 +1,13 @@
+import { MotoristaDuplicadoException } from "../exceptions/MotoristaDuplicadoException.js";
 import { AppError } from "../utils/AppError.js";
 
 export const middlewareDeErros = (err, req, res, next) => {
 	if (err instanceof AppError) {
 		return res.status(err.statusCode).json({ erro: err.message });
+	}
+
+	if (err instanceof MotoristaDuplicadoException) {
+		return res.status(409).json({ erro: err.message });
 	}
 
 	console.error(err);

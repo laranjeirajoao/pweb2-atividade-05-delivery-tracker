6d4b5b52c0b94f50cd3286ffc2cfa791ba615813
João Laranjeira <jpclr1@aluno.ifal.edu.br>
2026-04-12 00:57:12 -0300

    refactor: mudando o import para o novo repositorio de motoristas no arquivo de composicao de dependencias

--- src/routes/composicao-dependencias.js
+++ src/routes/composicao-dependencias.js
@@ -1,16 +1,13 @@
 // src/bootstrap.js
 import { EntregasController } from "../controllers/entregas.controller.js";
 import { MotoristasController } from "../controllers/motoristas.controller.js";
-import { Database } from "../database/database.js";
-// import { EntregasRepositoryInMemory as EntregasRepository } from "../repositories/entregas/entregas-in-memory.repository.js";
 import { EntregasRepositorySQL as EntregasRepository } from "../repositories/entregas/entregas-sql.repository.js";
-import { MotoristasRepositoryInMemory as MotoristasRepository } from "../repositories/motoristas/motoristas-in-memory.repository.js";
+import { MotoristasRepositorySQL as MotoristasRepository } from "../repositories/motoristas/motoristas-sql.repository.js";
 import { EntregasService } from "../services/entregas.service.js";
 import { MotoristasService } from "../services/motoristas.service.js";
 
-const database = new Database();
 const entregasRepo = new EntregasRepository();
-const motoristasRepo = new MotoristasRepository(database);
+const motoristasRepo = new MotoristasRepository();
 const entregasService = new EntregasService(entregasRepo, motoristasRepo);
 const motoristasService = new MotoristasService(motoristasRepo);
 const entregasController = new EntregasController(entregasService);

160c1e650c651fd734caf97878da1e771f3b90ba
João Laranjeira <jpclr1@aluno.ifal.edu.br>
2026-04-17 21:16:22 -0300

    refactor: remove a chamada pra migration manual, uma vez que agora o prisma que gerencia elas

--- src/server.js
+++ src/server.js
@@ -1,10 +1,8 @@
 import "dotenv/config";
 import app from "./app.js";
-import { runMigrationCreateTables } from "./migrations/run-migrations.js";
 
 const PORTA = process.env.PORT || 3000;
 
 app.listen(PORTA, () => {
-	runMigrationCreateTables();
 	console.log(`API iniciada na porta ${PORTA}`);
 });

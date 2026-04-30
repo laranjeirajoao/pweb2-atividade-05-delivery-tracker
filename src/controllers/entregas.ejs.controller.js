af9a6c0d82126d8e5aaf4267962da3a389cb49a8
João Laranjeira <jpclr1@aluno.ifal.edu.br>
2026-04-30 20:45:42 -0300

    chore: clona o controller de motoristas e entregas, para reutilizar a mesma logica ja implementada

---
+++ src/controllers/entregas.ejs.controller.js
@@ -0,0 +1,113 @@
+//
+export class EntregasControllerEjs {
+	constructor(service) {
+		this.service = service;
+
+		// Binding necessário para preservar o contexto de `this` nos handlers
+		this.listarTodos = this.listarTodos.bind(this);
+		this.listarEntregasPorStatusAgrupados =
+			this.listarEntregasPorStatusAgrupados.bind(this);
+		this.buscarPorId = this.buscarPorId.bind(this);
+		this.criar = this.criar.bind(this);
+		this.buscarHistoricoPorId = this.buscarHistoricoPorId.bind(this);
+		this.avancarStatus = this.avancarStatus.bind(this);
+		this.cancelarEntrega = this.cancelarEntrega.bind(this);
+		this.atribuirMotorista = this.atribuirMotorista.bind(this);
+	}
+
+	async listarTodos(req, res, next) {
+		try {
+			const { status, page, limit, createdDe, createdAte } = req.query;
+			const entregas = await this.service.listarTodos({
+				status,
+				page,
+				limit,
+				createdDe,
+				createdAte,
+			});
+			res.render("layouts/entregas/index", { entregas: entregas.data });
+		} catch (err) {
+			next(err);
+		}
+	}
+
+	async listarEntregasPorStatusAgrupados(req, res, next) {
+		try {
+			const entregas = await this.service.listarEntregasPorStatusAgrupados();
+			// res.json(entregas);
+			//todo
+		} catch (err) {
+			next(err);
+		}
+	}
+
+	async buscarPorId(req, res, next) {
+		try {
+			const entrega = await this.service.buscarPorId(Number(req.params.id));
+			// res.json(entrega);
+			// todo
+		} catch (err) {
+			next(err);
+		}
+	}
+
+	async criar(req, res, next) {
+		try {
+			const novaEntrega = await this.service.criar(req.body);
+			// res.status(201).json(novaEntrega);
+			//todo
+		} catch (err) {
+			next(err);
+		}
+	}
+
+	async avancarStatus(req, res, next) {
+		try {
+			const atualizado = await this.service.avancarStatus(
+				Number(req.params.id),
+			);
+			// res.json(atualizado);
+			// todo
+		} catch (err) {
+			next(err);
+		}
+	}
+
+	async cancelarEntrega(req, res, next) {
+		try {
+			const atualizado = await this.service.cancelarEntrega(
+				Number(req.params.id),
+			);
+			// res.json(atualizado);
+			// todo
+		} catch (err) {
+			next(err);
+		}
+	}
+
+	async buscarHistoricoPorId(req, res, next) {
+		try {
+			const historico = await this.service.buscarHistoricoPorId(
+				Number(req.params.id),
+			);
+			// res.json(historico);
+			// todo
+		} catch (err) {
+			next(err);
+		}
+	}
+
+	async atribuirMotorista(req, res, next) {
+		try {
+			const { motoristaId } = req.body;
+			const historico = await this.service.atribuirMotorista(
+				Number(req.params.id),
+				Number(motoristaId),
+			);
+			// res.json(historico);
+			// todo
+		} catch (err) {
+			next(err);
+		}
+	}
+}
---
+++ src/controllers/motoristas.ejs.controller.js
@@ -0,0 +1,72 @@
+//
+export class MotoristasControllerEjs {
+	constructor(service, entregasService) {
+		this.service = service;
+		this.entregasService = entregasService;
+
+		// Binding necessário para preservar o contexto de `this` nos handlers
+		this.listarTodos = this.listarTodos.bind(this);
+		this.buscarPorId = this.buscarPorId.bind(this);
+		this.criar = this.criar.bind(this);
+		this.buscarEntregas = this.buscarEntregas.bind(this);
+		this.listarMotoristasAtivos = this.listarMotoristasAtivos.bind(this);
+	}
+
+	async listarTodos(req, res, next) {
+		try {
+			const motoristas = await this.service.listarTodos();
+			// res.json(motoristas);
+			//todo
+		} catch (err) {
+			next(err);
+		}
+	}
+
+	async buscarPorId(req, res, next) {
+		try {
+			const motorista = await this.service.buscarPorId(
+				Number(req.params.id),
+			);
+			// res.json(motorista);
+			// todo
+		} catch (err) {
+			next(err);
+		}
+	}
+
+	async criar(req, res, next) {
+		try {
+			const novoMotorista = await this.service.criar(req.body);
+			// res.status(201).json(novoMotorista);
+			//todo
+		} catch (err) {
+			next(err);
+		}
+	}
+
+	async buscarEntregas(req, res, next) {
+		try {
+			const { status } = req.query;
+			const motoristaId = Number(req.params.id);
+			const entregas = await this.entregasService.listarTodos(
+				status,
+				motoristaId,
+			);
+			// res.json(entregas);
+			//todo
+		} catch (err) {
+			next(err);
+		}
+	}
+
+	async listarMotoristasAtivos(req, res, next) {
+		try {
+			const entregas =
+				await this.service.listarMotoristasComEntregasAtivas();
+			// res.json(entregas);
+			//todo
+		} catch (err) {
+			next(err);
+		}
+	}
+}

c6ce5c91c044f790c45788a5b0742cebf115d087
João Laranjeira <jpclr1@aluno.ifal.edu.br>
2026-04-30 20:46:25 -0300

    feat: cria os novos exports do controller para o ejs

--- src/routes/composicao-dependencias.js
+++ src/routes/composicao-dependencias.js
@@ -1,6 +1,8 @@
 // src/bootstrap.js
 import { EntregasController } from "../controllers/entregas.controller.js";
+import { EntregasControllerEjs } from "../controllers/entregas.ejs.controller.js";
 import { MotoristasController } from "../controllers/motoristas.controller.js";
+import { MotoristasControllerEjs } from "../controllers/motoristas.ejs.controller.js";
 import { EntregasRepositoryPrisma as EntregasRepository } from "../repositories/entregas/entregas-prisma.repository.js";
 import { MotoristasRepositoryPrisma as MotoristasRepository } from "../repositories/motoristas/motoristas-prisma.repository.js";
 import { EntregasService } from "../services/entregas.service.js";
@@ -16,4 +18,15 @@ const motoristasController = new MotoristasController(
 	entregasService,
 );
 
-export { entregasController, motoristasController };
+const entregasControllerEjs = new EntregasControllerEjs(entregasService);
+const motoristasControllerEjs = new MotoristasControllerEjs(
+	motoristasService,
+	entregasService,
+);
+
+export {
+	entregasController,
+	entregasControllerEjs,
+	motoristasController,
+	motoristasControllerEjs,
+};

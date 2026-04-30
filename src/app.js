5504ff06a75d9c5f7c13dd1ca44f379ffaf7f4d5
João Laranjeira <jpclr1@aluno.ifal.edu.br>
2026-04-30 20:48:39 -0300

    feat: configuracao basica do ejs

--- src/app.js
+++ src/app.js
@@ -1,11 +1,19 @@
 import express from "express";
+import { dirname, join } from "node:path";
+import { fileURLToPath } from "node:url";
 import { middlewareDeErros } from "./middlewares/error.middleware.js";
 import router from "./routes/index.js";
 
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const app = express();
+
+app.set("view engine", "ejs");
+app.set("views", join(__dirname, "views"));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(join(__dirname, "..", "public")));
 
 app.use(router);
 

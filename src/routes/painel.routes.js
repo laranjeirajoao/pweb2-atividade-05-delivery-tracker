9806536b364af15ab550515403eff7dd694c6add
João Laranjeira <jpclr1@aluno.ifal.edu.br>
2026-05-05 15:03:40 -0300

    fix: atualiza o layout usado na pagina inicial

--- src/routes/painel.routes.js
+++ src/routes/painel.routes.js
@@ -3,7 +3,7 @@ import { Router } from "express";
 const router = Router();
 
 router.get("/", (req, res) => {
-	res.render("index");
+	res.render("layouts/painel/index");
 });
 
 export default router;

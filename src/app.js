3c07ab95949e9c2f4a8c89d09f5318af74c433d0
João Laranjeira <jpclr1@aluno.ifal.edu.br>
2026-05-05 14:52:22 -0300

    feat: adiciona methodOverride, flash, expressEjsLayout e expressValidator

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import flash from "connect-flash";
 import express from "express";
+import expressLayouts from "express-ejs-layouts";
 import session from "express-session";
+import methodOverride from "method-override";
 import { dirname, join } from "node:path";
 import { fileURLToPath } from "node:url";
 import { middlewareDeErros } from "./middlewares/error.middleware.js";
@@ -10,35 +12,41 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 
-app.use(
-	session({
-		secret: process.env.SESSION_SECRET || "segredo-dev",
-		resave: false,
-		saveUninitialized: false,
-		cookie: { secure: false }, // true em produção com HTTPS
-	}),
-);
-
-app.use(flash());
-
 app.set("view engine", "ejs");
 app.set("views", join(__dirname, "views"));
+
+app.use(expressLayouts);
+app.set("layout", "layouts/base");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(join(__dirname, "..", "public")));
 
-app.use(router);
+app.use(
+	session({
+		secret: process.env.SESSION_SECRET || "segredo-dev",
+		resave: false,
+		saveUninitialized: false,
+		cookie: { secure: false },
+	}),
+);
 
-// Middleware de erro — DEVE ter 4 parâmetros
-app.use(middlewareDeErros);
+app.use(flash());
 
 app.use((req, res, next) => {
 	res.locals.flash = {
 		sucesso: req.flash("sucesso"),
 		erro: req.flash("erro"),
 	};
+
 	next();
 });
+
+app.use(methodOverride("_method"));
+
+app.use(router);
+
+// Middleware de erro — DEVE ter 4 parâmetros
+app.use(middlewareDeErros);
 
 export default app;

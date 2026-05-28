825febe32a2e23fbb8d83fa7951adf9fe790bb11
João Laranjeira <jpclr1@aluno.ifal.edu.br>
2026-05-27 22:22:31 -0300

    feat: adiciona middleware de autorizacao para permitir apenas o papel de GESTOR na rota: POST /api/motoristas

--- src/routes/motoristas.routes.js
+++ src/routes/motoristas.routes.js
@@ -1,10 +1,17 @@
 import { Router } from "express";
+import { autenticar } from "../middlewares/autenticar.middleware.js";
 import { validarCriacaoMotorista } from "../middlewares/validacao.middleware.js";
+import { RoleEnum } from "../utils/RoleEnum.js";
 import { motoristasController } from "./composicao-dependencias.js";
 
 const router = Router();
 
-router.post("/", validarCriacaoMotorista, motoristasController.criar);
+router.post(
+	"/",
+	autenticar(RoleEnum.GESTOR),
+	validarCriacaoMotorista,
+	motoristasController.criar,
+);
 router.get("/", motoristasController.listarTodos);
 router.get("/:id", motoristasController.buscarPorId);
 router.get("/:id/entregas", motoristasController.buscarEntregas);

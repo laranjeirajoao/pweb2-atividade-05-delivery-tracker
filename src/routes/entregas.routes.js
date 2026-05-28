a0be75147931726b0aea99fa3e855ba9ed825d8c
João Laranjeira <jpclr1@aluno.ifal.edu.br>
2026-05-27 22:19:05 -0300

    feat: adiciona middleware de autorizacao para permitir apenas o papel de GESTOR na rota: PATCH /api/entregas/:id/cancelar

--- src/routes/entregas.routes.js
+++ src/routes/entregas.routes.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
+import { autenticar } from "../middlewares/autenticar.middleware.js";
 import {
 	validarCriacaoEntrega,
 	validarDatasNaQuery,
 } from "../middlewares/validacao.middleware.js";
+import { RoleEnum } from "../utils/RoleEnum.js";
 import { entregasController } from "./composicao-dependencias.js";
 
 const router = Router();
@@ -11,7 +13,11 @@ router.post("/", validarCriacaoEntrega, entregasController.criar);
 router.get("/", validarDatasNaQuery, entregasController.listarTodos);
 router.get("/:id", entregasController.buscarPorId);
 router.patch("/:id/avancar", entregasController.avancarStatus);
-router.patch("/:id/cancelar", entregasController.cancelarEntrega);
+router.patch(
+	"/:id/cancelar",
+	autenticar(RoleEnum.GESTOR),
+	entregasController.cancelarEntrega,
+);
 router.get("/:id/historico", entregasController.buscarHistoricoPorId);
 router.patch("/:id/atribuir", entregasController.atribuirMotorista);
 
